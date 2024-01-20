546ecdf3a63377ac9e6a1a60d46fdcbbf27e1a26
Alexander Korobov <dktwho@mail.ru>
2024-01-20 12:12:13 +0300

    feat: add nodemon, preform to ts, add folder dist for js file

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req: any, res: any) {
     res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Start server on port 3000!');
 });
